bcf7c8a4ec8166010d86a30518d233e3a1fa845e
unknown <mohammedlokhand2021@gmail.com>
2023-03-07 11:18:49 +0530

    whatsapp

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles,{layout} from '../style'
 import Button from './Button'
+import { Link } from 'react-router-dom'
 const CTA = () =>  (
     
     <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] relative z-[1] `}>
@@ -10,7 +11,9 @@ const CTA = () =>  (
       </div>
 
       <button className={` ${styles.flexCenter}  sm:ml-10 ml-0 sm:mt-0 mt-10`}>
+        <Link to="https://wa.me/8849493915">
         <Button className='rounded-[20px]'/>
+        </Link>
       </button>
     </section>
     
