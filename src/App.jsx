e06eae5a4cdad2f070d1394594cbd3918426db07
unknown <mohammedlokhand2021@gmail.com>
2023-03-11 22:38:47 +0530

    error

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const App = () => (
         <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
           <div className={`${styles.boxWidth}`}>
             <Business />
-  
+  <CardDeal/>
             <Calendy />
             <Testimonials />
             <CTA />
