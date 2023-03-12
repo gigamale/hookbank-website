3e2766b7c62a288a6a912baeaf152ba7f9bfb195
unknown <mohammedlokhand2021@gmail.com>
2023-03-12 13:22:34 +0530

    footr

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,10 +7,10 @@ import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <section className={` ${styles.flexCenter} ${styles.paddingY} flex-col md:border-b-4 md:border-indigo-500`}>
+    <section className={` ${styles.flexCenter} ${styles.paddingY} flex-col md:border-b-3 md:border-indigo-500`}>
       <div className={`${styles.flexStart} flex-col md:flex-row flex-col w-full`}>
-      <div className={`flex-[1] flex flex-col md:justify-start md:items-start items-center mr-10 w-full `}>
-        <img src={logo} className='h-[72.14] w-[266px]  1'/>
+      <div className={`flex-[1] flex flex-col md:justify-start md:items-start items-center mr-3 w-full `}>
+        <img src={logo} className='md:h-[72.14] md:w-[266px]  md:block hidden' />
          <p className={`md:block hidden font-poppins font-semibold text-white text-[25px] leading-[30.8px] mt-7`} style={{color:'white'}}>So don't let your message get lost <br/>in the noise  <span style={{fontWeight:'bold',fontSize:'25px'}} className='text-gradient'>connect with us today.</span></p>
       </div>
 
